acb40594c5663682c16810955cb8aa51a81e69f6
alessia-mertolini <alessia.mertolini@studio.unibo.it>
2026-02-22 22:15:31 +0100

    aggiunta della X di rimozione
    in opere e sezioni

--- public/js/add-section.js
+++ public/js/add-section.js
@@ -7,21 +7,27 @@ function renderAddSection() {
     // Creazione del div della nuova sezione
     const sectionDiv = document.createElement('div');
     sectionDiv.className = "section-block";
-    sectionDiv.style = "border: 2px solid #007bff; padding: 15px; margin: 10px 0;";
+    sectionDiv.style = "border: 2px solid #007bff; padding: 15px; margin: 10px 0; position: relative";
     sectionDiv.id = `section-${sectionCount}`;
 
     sectionDiv.innerHTML = `
-        <h3>Sezione ${sectionCount}</h3>
-        <label>Titolo sezione:</label><br>
-        <input type="text" name="sectionName[]" required><br>
-        
-        <label>Path immagine sezione:</label><br>
-        <input type="text" name="sectionImage[]"><br>
-        
-        <div id="works-container-${sectionCount}" style="margin-left: 20px; border-left: 1px dashed #ccc;">
-            </div>
-        
-        <button type="button" onclick="renderAddWork(${sectionCount})">+ Aggiungi Opera a questa sezione</button>
+      <button type="button" 
+        onclick="this.parentElement.remove()" 
+        style="position: absolute; right: 10px; top: 10px; color: red; cursor: pointer;">
+        ✖
+      </button>
+
+      <h3>Sezione ${sectionCount}</h3>
+      <label>Titolo sezione:</label><br>
+      <input type="text" name="sectionName[]" required><br>
+      
+      <label>Path immagine sezione:</label><br>
+      <input type="text" name="sectionImage[]"><br>
+      
+      <div id="works-container-${sectionCount}" style="margin-left: 20px; border-left: 1px dashed #ccc;">
+        </div>
+      
+      <button type="button" onclick="renderAddWork(${sectionCount})">+ Aggiungi Opera a questa sezione</button>
     `;
 
     container.appendChild(sectionDiv);
